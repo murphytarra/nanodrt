a072f0db8055ae1277ec2252aa3d6a2a8ecc8a56
murphytarra <murphytarra@gmail.com>
2024-05-27 20:31:47 +0100

    added plotting functionality

--- nanodrt/fitting/regularization.py
+++ nanodrt/fitting/regularization.py
@@ -6,82 +6,122 @@ import jaxopt
 import dataclasses
 
 # Ask about typechecking again? Look up...
-from nanodrt.drt_solver.drt import DRT
 from nanodrt.drt_solver.measurements import ImpedenceMeasurement
-from nanodrt.fitting.fits import FittedSpectrum
-from nanodrt.drt_solver.simulaton import Simulation
-from nanodrt.drt_solver.solvers import RBFSolver
 
-class GCV(eqx.Module): 
-    """ 
+
+class GCV(eqx.Module):
+    """
     Object which computes the regularisation parameter by minimizing the GCV function
     """
+
+    # Object containing Measurement information
     measurement: ImpedenceMeasurement
 
-    A_re: jnp.ndarray 
+    # Real component of A matrices
+    A_re: jnp.ndarray
 
+    # Imaginary component of A matrices
     A_im: jnp.ndarray
 
-    M: jnp.ndarray  = dataclasses.field(default=None)  # type: ignore
+    # Matrix in normalisation
+    M: jnp.ndarray = dataclasses.field(default=None)  # type: ignore
 
-    init_lbd: float  = dataclasses.field(default=None)  # type: ignore
-    
+    # Initial value of lbd used in the optimisation process
+    init_lbd: float = dataclasses.field(default=None)  # type: ignore
+
+    def __post_init__(self):
+        self.validate_init()
+
     @eqx.filter_jit
     def __call__(self) -> float:
 
         solver = jaxopt.LBFGS(fun=self.compute_GCV, maxiter=50000)
-        res = solver.run(init_params=jnp.array(self.init_lbd), A_re=self.A_re, A_im=self.A_im, measurement=self.measurement,  M=jnp.eye(self.A_re.shape[1] + 2))
+        res = solver.run(
+            init_params=jnp.array(self.init_lbd),
+            A_re=self.A_re,
+            A_im=self.A_im,
+            measurement=self.measurement,
+            M=jnp.eye(self.A_re.shape[1] + 2),
+        )
         param, state = res
         return param
 
     @eqx.filter_jit
-    def compute_GCV(self, init_params, A_re, A_im, measurement, M):
-    
+    def compute_GCV(self, init_params, A_re, A_im, measurement, M) -> float:
         """
-        This function computes the score for the generalized cross-validation (GCV) approach.
-        Reference: G. Wahba, A comparison of GCV and GML for choosing the smoothing parameter in the generalized spline smoothing problem, Ann. Statist. 13 (1985) 1378–1402.
-        Inputs: 
-            log_lambda: regularization parameter
-            A_re: discretization matrix for the real part of the impedance
-            A_im: discretization matrix for the real part of the impedance
-            Z_re: vector of the real parts of the impedance
-            Z_im: vector of the imaginary parts of the impedance
-            M: differentiation matrix 
-        Output:
-            GCV score
+        Compute the Generalized Cross Validation Metric for a given lambda value.
+
+        Args:
+            init_params (float): Value of lambda used
+            A_re (jnp.ndarray): Real component of A matrix used in DRT calculation
+            A_im (jnp.ndarray): Imaginary Component of A matrix used in DRT Calculation
+            measurement (ImpedanceMeasurement): Object containing measurement information
+            M (Matrix): Used in Regularisation
+
+        Returns:
+            float: Value of GCV
         """
-        
+
+        # Ensure that the lambda value is positive
         lambda_value = jnp.exp(init_params)
-        Z = jnp.hstack(( measurement.Z_re,measurement.Z_im)) # stacked impedance
 
-        n_cv = Z.shape[0] # n_cv = 2*N_freqs with N_freqs the number of EIS frequencies
-        n_t = A_re.shape[1]
+        # Create the vector of measured impedences
+        Z = jnp.hstack((measurement.Z_re, measurement.Z_im))  # stacked impedance
 
-        n_cv2 = int(n_cv/2)
+        # Shape of impedance
+        n_cv = Z.shape[0]  # n_cv = 2*N_freqs with N_freqs the number of EIS frequencies
+
+        # Create arrays of zeros and ones
+        n_cv2 = int(n_cv / 2)
         ones = jnp.ones(n_cv2)
         zeros = jnp.zeros(n_cv2)
 
+        # Create total A matrix
         A_top = jnp.vstack((zeros, ones, A_re.T))
-        A_bottom = jnp.vstack((2.*jnp.pi*measurement.f, zeros, A_im.T))
+        A_bottom = jnp.vstack((2.0 * jnp.pi * measurement.f, zeros, A_im.T))
         A = jnp.hstack((A_top, A_bottom))
-        #print(A.shape) # size (N + 2, 2M)
-            
-        #A = jnp.concatenate((A_re, A_im), axis = 0) # matrix A with A_re and A_im ; see (5) in [4]
-    
-        n_cv = Z.shape[0] # n_cv = 2*N_freqs with N_freqs the number of EIS frequencies
-        
-        A_agm = A@A.T + lambda_value*M # see (13) in [4]
-        
-        #print(A_agm.shape)
-        L_agm = jnp.linalg.cholesky(A_agm) # Cholesky transform to inverse A_agm
+
+        # Determine inverse calculation used in GCV Calculation
+        A_agm = A @ A.T + lambda_value * M  # see (13) in [4]
+        L_agm = jnp.linalg.cholesky(A_agm)  # Cholesky transform to inverse A_agm
         inv_L_agm = jnp.linalg.inv(L_agm)
-        inv_A_agm = inv_L_agm.T@inv_L_agm # inverse of A_agm
-        A_GCV = A.T@inv_A_agm@A  # see (13) in [4]
-        # GCV score; see (13) in [4]
-        GCV_num = 1/n_cv*jnp.linalg.norm((jnp.eye(n_cv)-A_GCV)@Z)**2 # numerator
-        GCV_dom = (1/n_cv*jnp.trace(jnp.eye(n_cv)-A_GCV))**2 # denominator
-        
-        GCV_score = GCV_num/GCV_dom
+        inv_A_agm = inv_L_agm.T @ inv_L_agm  # inverse of A_agm
+        A_GCV = A.T @ inv_A_agm @ A  # see (13) in [4]
+
+        # GCV score
+        GCV_num = (
+            1 / n_cv * jnp.linalg.norm((jnp.eye(n_cv) - A_GCV) @ Z) ** 2
+        )  # numerator
+        GCV_dom = (1 / n_cv * jnp.trace(jnp.eye(n_cv) - A_GCV)) ** 2  # denominator
+
+        GCV_score = GCV_num / GCV_dom
         return GCV_score
 
+    def validate_init(self):
+        """
+        Validate the initialization parameters to ensure they meet the necessary criteria.
+        """
+        if self.A_re.ndim != 2:
+            raise ValueError(
+                f"A_re must be a 2-dimensional array, but got {self.A_re.ndim} dimensions"
+            )
+        if self.A_im.ndim != 2:
+            raise ValueError(
+                f"A_im must be a 2-dimensional array, but got {self.A_im.ndim} dimensions"
+            )
+        if self.M is not None and self.M.shape != (
+            self.A_re.shape[1] + 2,
+            self.A_re.shape[1] + 2,
+        ):
+            raise ValueError(
+                f"M must be of shape {(self.A_re.shape[1] + 2, self.A_re.shape[1] + 2)}, but got {self.M.shape}"
+            )
 
+    def __repr__(self):
+        return (
+            f"GCV(measurement={self.measurement}, "
+            f"A_re=Array of shape {self.A_re.shape}, "
+            f"A_im=Array of shape {self.A_im.shape}, "
+            f"M=Array of shape {None if self.M is None else self.M.shape}, "
+            f"init_lbd={self.init_lbd})"
+        )
